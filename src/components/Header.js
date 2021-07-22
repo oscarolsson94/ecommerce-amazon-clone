773d5677f440330af01b74a4e8f0c51648d3b81b
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-07-22 09:39:55 +0200

    building header and whitelist Image link domains

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,21 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header>
+      {/* Top nav */}
+      <div>
+        <div>
+          <Image
+            src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+            width={150}
+            height={40}
+          />
+        </div>
+      </div>
+
+      {/* Bottom nav */}
+      <div></div>
+    </header>
   );
 };
 
