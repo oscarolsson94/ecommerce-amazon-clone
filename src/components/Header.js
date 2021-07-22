002754449ec0f7e5121ef07cfbc8f4c7f02b5dee
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-07-22 10:04:29 +0200

    logo part done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,18 @@
 import React from "react";
+import Image from "next/image";
 
 const Header = () => {
   return (
     <header>
       {/* Top nav */}
-      <div>
-        <div>
+      <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+        <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
           <Image
             src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
             width={150}
             height={40}
+            objectFit="contain"
+            className="cursor-pointer"
           />
         </div>
       </div>
